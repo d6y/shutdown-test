8fc1bd5545f27b8528a4b321864a8ee062e8330f
Richard Dallaway <richard@spiralarm.com>
2011-03-07 16:55:56 +0000

    Added commet example.  Observation: this is enough to reproduce the problem.

---
+++ src/main/scala/com/example/comet/CometActor.scala
@@ -0,0 +1,46 @@
+// JNDI: This class taken from: http://www.assembla.com/wiki/show/liftweb/Comet_Support
+package com.example {
+package comet {
+
+import net.liftweb._
+import http._
+import SHtml._ 
+import net.liftweb.common.{Box, Full}
+import net.liftweb.util._
+import net.liftweb.actor._
+import net.liftweb.util.Helpers._
+import net.liftweb.http.js.JsCmds.{SetHtml}
+import net.liftweb.http.js.JE.Str
+
+
+
+class CometMessage extends CometActor {
+	
+  override def defaultPrefix = Full("comet")
+		
+  def render = bind("message" -> <span id="message">Whatever you feel like returning</span>)
+		
+	// Users/richard/tmp/shutdown-test/src/main/scala/com/example/comet/CometActor.scala:22: value ActorPing in package util is deprecated: Use Schedule
+  ActorPing.schedule(this, Message, 10000L)
+		
+	/*
+	One for another day, but....
+	
+[error] /Users/richard/tmp/shutdown-test/src/main/scala/com/example/comet/CometActor.scala:24: type mismatch;
+[error]  found   : net.liftweb.http.js.JE.Str
+[error]  required: scala.xml.NodeSeq
+[error]       partialUpdate(SetHtml("message", Str("updated: " + timeNow.toString)))
+	*/	
+		
+  override def lowPriority : PartialFunction[Any,Unit] = {
+    case Message => {
+      partialUpdate(SetHtml("message", scala.xml.Text("updated: " + timeNow.toString)))
+      //partialUpdate(SetHtml("message", Str("updated: " + timeNow.toString)))
+      ActorPing.schedule(this, Message, 10000L)
+    }
+  }
+}
+case object Message
+
+}
+}
